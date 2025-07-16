0d08acf324fa3afac400a007e7a75ac60ef39b19
vishalkakiyan <vishalkakiyan@gmail.com>
2025-07-16 23:14:41 +0530

    database

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-import mongoose from 'mongoose'
-import {DB_NAME} from './constants'
+import dotenv from 'dotenv'
+import connectDB from './db/index.js'
+dotenv.config({path:'./env'})
+
+connectDB();
+
+
+
+
+
+// import express from 'express'
+// const app=express();
+// (async()=>{
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on('error',(err)=>{
+//             console.log(err);
+//             throw err;
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`The server is listening on ${process.env.PORT}`)
+//         })
+//     } catch (error) {
+//         console.error("ERROR:",error);
+//         throw error;
+//     }
+// })()
