71591777471052ed2a91960c801a5198139f1a1f
vishalkakiyan <vishalkakiyan@gmail.com>
2025-07-31 22:12:38 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -15,4 +15,10 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//import routes
+import userRouter from './routes/user.routes.js'
+
+//import declaration
+app.use('/api/v1/users',userRouter)
+
 export { app }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv'
 import connectDB from './db/index.js'
+import express from 'express'
+import {app} from './app.js'
 dotenv.config({path:'./env'})
 
 connectDB()
